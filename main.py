9bdc748ea69dcd1d84cb370e38edccd489c14797
Mikhail-U-K <programming2020>
2021-07-21 15:47:10 +0300

    simple print progs

---
+++ main.py
@@ -0,0 +1,22 @@
+def print_hi():
+    print("Python is awesome!")
+    var = "first string"
+    print(var)
+    var = 2 + 9
+    print(var)
+
+
+def some_stuff():
+    print("Monty Python's flying circus")
+    print('Monty Python\'s flying circus')
+    print("String")
+    print('String')
+    print('''Str 
+    ... ing''')
+    print('Str"ing')
+
+
+if __name__ == '__main__':
+    print_hi()
+    some_stuff()
+
